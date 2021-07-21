1d4dc1039110231ac7572bdfc93a51b7597cae52
unknown <commandodev@protonmail.com>
2021-07-21 11:21:10 +0200

    Implement checksum generation

--- Simple Banking System/task/src/banking/Account.java
+++ Simple Banking System/task/src/banking/Account.java
@@ -9,17 +9,37 @@ public class Account {
     private class Card {
         private final int BIN = 400000;
         private final int accountIdentifier;
-        private final int CHECKSUM = 0;
+        private final int CHECKSUM;
         private final int PIN;
 
         public Card() {
             System.out.println("Your card has been created");
             accountIdentifier = new Random().nextInt(999_999_999 - 100_000_000 + 1) + 100_000_000;
+            CHECKSUM = generateChecksum();
             System.out.println("Your card number:");
             System.out.println(getCardNumber());
             PIN = generatePIN();
-            System.out.println("Your card PIN");
+            System.out.println("Your card PIN:");
             System.out.println(PIN);
+        }
+
+        private int generateChecksum() {
+            String currentNumber = "" + BIN + accountIdentifier;
+            int sum = 0;
+            for (int i = 0; i < currentNumber.length(); i++) {
+                int num = Character.getNumericValue(currentNumber.charAt(i));
+                if ( (i + 1) % 2 != 0) {
+                    num *= 2;
+                    if (num > 9) {
+                        sum += num - 9;
+                    } else {
+                        sum += num;
+                    }
+                } else {
+                    sum += num;
+                }
+            }
+            return 10 - (sum % 10) != 10 ? 10 - (sum % 10) : 0;
         }
 
         private String getCardNumber() {
